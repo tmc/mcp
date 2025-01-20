f84fd115e0ec2a7f9eb5d42e2ec518d43b7eb473
Travis Cline <travis.cline@gmail.com>
2025-01-20 03:13:10 -0800

    refactor: move client code to internal/mcpclient package

---
+++ internal/mcpclient/transport.go
@@ -0,0 +1,28 @@
+package mcpclient
+
+import (
+	"context"
+	"io"
+	"os"
+)
+
+// StdioTransport implements Transport over stdin/stdout
+type StdioTransport struct {
+	ctx context.Context
+	in  io.Reader
+	out io.Writer
+}
+
+// NewStdioTransport creates a new stdio transport
+func NewStdioTransport(ctx context.Context) *StdioTransport {
+	return &StdioTransport{
+		ctx: ctx,
+		in:  os.Stdin,
+		out: os.Stdout,
+	}
+}
+
+func (t *StdioTransport) Read(p []byte) (n int, err error)  { return t.in.Read(p) }
+func (t *StdioTransport) Write(p []byte) (n int, err error) { return t.out.Write(p) }
+func (t *StdioTransport) Close() error                      { return nil }
+func (t *StdioTransport) Context() context.Context          { return t.ctx }

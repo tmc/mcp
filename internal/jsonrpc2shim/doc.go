1da4d88024538f41879260705f1b40e37ffe908b
Travis Cline <travis.cline@gmail.com>
2025-06-06 19:09:14 -0700

    internal/jsonrpc2shim: add compatibility layer between jsonrpc2 implementations
    Add a new internal package that provides compatibility shims between
    golang.org/x/tools/internal/jsonrpc2_v2 and golang.org/x/exp/jsonrpc2.
    This enables easier migration and interoperability between code using
    different versions of the JSON-RPC2 implementation.
    
    The implementation includes connection handling, type forwarding,
    ID management, and standard error definitions.

---
+++ internal/jsonrpc2shim/doc.go
@@ -0,0 +1,2 @@
+// Package jsonrpc2shim exists to shim between golang.org/x/tools/internal/jsonrpc2_v2 and golang.org/x/exp/jsonrpc2 to ease  use of code that relies on the former.
+package jsonrpc2shim
